6f9e2450509e5df813d92af9b6b7bf9c86cfcb63
Clayton Hite <clay054@aol.com>
2022-10-18 09:50:49 -0400

    Initial Commit

---
+++ WebsiteDatabase/ClassSchedule.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ClassSchedule]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ClassName] VARCHAR(200) NULL, 
+    [ClassStartDate] VARCHAR(20) NULL, 
+    [ClassEndDate] VARCHAR(20) NULL, 
+    [ClassStartTime] VARCHAR(20) NULL, 
+    [ClassEndTime] VARCHAR(20) NULL, 
+    [ClassInformation] VARCHAR(2000) NULL, 
+    [ClassHost] VARCHAR(100) NULL, 
+    [ClassAddress] VARCHAR(100) NULL
+)
---
+++ WebsiteDatabase/Users.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(100) NULL, 
+    [MiddleName] VARCHAR(100) NULL, 
+    [LastName] VARCHAR(100) NULL, 
+    [AddressLine1] VARCHAR(100) NULL, 
+    [AddressLine2] VARCHAR(100) NULL, 
+    [City] VARCHAR(100) NULL, 
+    [State] VARCHAR(100) NULL, 
+    [Zip] VARCHAR(100) NULL, 
+    [Phone] VARCHAR(100) NULL, 
+    [Email] VARCHAR(100) NULL, 
+    [Account] INT NOT NULL, 
+    CONSTRAINT [FK_Users_ToUserAccounts] FOREIGN KEY (Account) REFERENCES [UserAccounts]([Id]) 
+)
