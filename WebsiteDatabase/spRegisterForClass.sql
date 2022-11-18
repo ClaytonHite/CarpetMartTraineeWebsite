2894bcea7d919f56a070a53c3133d57a41ec9769
Clayton Hite <clay054@aol.com>
2022-11-18 11:12:03 -0500

    Prevented duplicate registries from trainees when registering for class.

--- WebsiteDatabase/spRegisterForClass.sql
+++ WebsiteDatabase/spRegisterForClass.sql
@@ -8,5 +8,14 @@ AS
 	SET @UserID = (SELECT us.Id FROM Users us WHERE us.MSId = @MSId)
 	SET @ClassID = (SELECT cs.Id FROM ClassSchedule cs WHERE cs.ClassName = @ClassName)
 
+	IF EXISTS (SELECT tr.ClassId, tr.UserId FROM TraineesRegisteredForClasses tr
+	WHERE tr.ClassId = @ClassID AND tr.UserId = @UserID)
+BEGIN
+	RETURN 0
+END
+ELSE
+BEGIN
 	INSERT INTO TraineesRegisteredForClasses (ClassId, UserId)
 	VALUES (@ClassID, @UserID)
+	RETURN 1
+END
